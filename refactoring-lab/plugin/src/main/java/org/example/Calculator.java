873f26531eb21f286fb3c593fdc2aa6bf83d022f
imanesoufli <iimanesoufli@gmail.com>
2026-05-07 00:00:08 +0100

    feat: add Calculator class with ratio calculation and result printing methods

---
+++ refactoring-lab/plugin/src/main/java/org/example/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+
+    public double calculateRatio(double firstNumber, double secondNumber) {
+
+        double sum = firstNumber + secondNumber;
+        double product = firstNumber * secondNumber;
+
+        return sum / product;
+    }
+
+    public void printResult(double result) {
+        System.out.println("Result: " + result);
+    }
+}
